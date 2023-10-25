ecd568b05e0e387969e98d660cae380d0f6c2ad5
Mihail Tudos <mihail.tudos@idwalmarine.com>
2023-10-25 22:22:20 +0100

    completed controllers

--- bookstore/pkg/controllers/BooksController.go
+++ bookstore/pkg/controllers/BooksController.go
@@ -1,23 +1,76 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/tudosm/go-projects/bookstore/pkg/models"
+	"github.com/tudosm/go-projects/bookstore/pkg/utils"
+	"net/http"
+	"strconv"
+)
+
+var NewBook models.Book
 
 func CreateBook(w http.ResponseWriter, r *http.Request) {
-
+	CreateBook := &models.Book{}
+	utils.ParseBody(r, CreateBook)
+	b := CreateBook.CreateBook()
+	res, _ := json.Marshal(b)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(res)
 }
 
 func GetBooks(w http.ResponseWriter, r *http.Request) {
-
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(res)
 }
 
 func ShowBooks(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
 
+	bookId, _ := strconv.Atoi(params["bookId"])
+	book, _ := models.GetBookById(bookId)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	res, _ := json.Marshal(book)
+
+	_, _ = w.Write(res)
 }
 
 func UpdateBooks(w http.ResponseWriter, r *http.Request) {
+	var updatedBook = &models.Book{}
+	utils.ParseBody(r, updatedBook)
+	params := mux.Vars(r)
+	bookId, _ := strconv.Atoi(params["bookId"])
 
+	book, db := models.GetBookById(bookId)
+	if updatedBook.Name != "" {
+		book.Name = updatedBook.Name
+	}
+	if updatedBook.Author != "" {
+		book.Author = updatedBook.Author
+	}
+	if updatedBook.Publication != "" {
+		book.Publication = updatedBook.Publication
+	}
+
+	db.Updates(book)
 }
 
 func DeleteBooks(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
 
+	bookId, _ := strconv.Atoi(params["bookId"])
+	book := models.DeleteBook(bookId)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	res, _ := json.Marshal(book)
+
+	_, _ = w.Write(res)
 }
--- bookstore/pkg/models/Book.go
+++ bookstore/pkg/models/Book.go
@@ -19,3 +19,26 @@ func init() {
 	db = config.GetDB()
 	_ = db.AutoMigrate(&Book{})
 }
+
+func (b *Book) CreateBook() *Book {
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+func DeleteBook(Id int) Book {
+	var book Book
+	db.Where("ID=?", Id).Delete(book)
+	return book
+}
